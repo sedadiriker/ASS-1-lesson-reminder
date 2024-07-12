df78c01a51ed28d8f7f6cda6cf942eb565932cfd
seda <seda.diriker@gmail.com>
2024-07-12 22:46:58 +0300

    add clear undo state

--- src/components/LessonCard/LessonCard.jsx
+++ src/components/LessonCard/LessonCard.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { data } from "../../helper/data";
 import LessonCardStyle from "./lesson.module.css";
 
 const LessonCard = () => {
+  const [clear, setClear] = useState(false)
+
+  const clickClear = () => {
+    setClear(!clear)
+  }
   return (
     <div className={LessonCardStyle.container}>
-      <h2 className={LessonCardStyle.title}>6 lessons today</h2>
+     {
+      clear ? (
+        <>
+        <p className={LessonCardStyle.title}>0 lessons today</p>
+        <button onClick={clickClear}>Undo</button>
+
+        </>
+      ):(
+        <>
+         <h2 className={LessonCardStyle.title}>6 lessons today</h2>
       <div className={LessonCardStyle.lessons}>
         {data.map(({ name, hour, image }) => (
           <div className={LessonCardStyle.lessonCard}>
@@ -22,7 +36,10 @@ const LessonCard = () => {
           </div>
         ))}
       </div>
-      <button>Clear List</button>
+      <button onClick={clickClear}>Clear List</button>
+        </>
+      )
+     }
     </div>
   );
 };
